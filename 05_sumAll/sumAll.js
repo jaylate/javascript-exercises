0045f091390f9e5c74a96032de7b080c17c57a48
jaylate <178095084+jaylate@users.noreply.github.com>
2024-08-17 18:03:24 +0300

    Complete first seven exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0)
+	return "ERROR";
+    let out = 0;
+    if (start < end) {
+	for (let i = start; i <= end; i++) out += i;
+    } else {
+	for (let i = end; i <= start; i++) out += i;
+	console.log(out);
+    }
+    return out;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+    return Number(((deg - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+    return Number((deg * 9 / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
